983b60d21502ab404d7f1a2dae07282aea527c26
otomdee <otomdaniel65@gmail.com>
2025-08-06 14:51:04 +0100

    add solution to Advanced Challenge 3

---
+++ Foundry_CU_challenges/src/AdvancedThree.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+contract AdvancedThree {
+    function getNumberr() public pure returns (uint128) {
+        return uint128(99);
+    }
+
+    function getOwner() public pure returns (address) {
+        address owner = 0x645b4605dC74e8eFF38d3FD26C3cabA853ABbF61;
+        return owner;
+    }
+}
---
+++ Foundry_CU_challenges/test/AdvancedThreeTest.t.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {Test, console} from "forge-std/Test.sol";
+import {AdvancedThree} from "../src/AdvancedThree.sol";
+import {LessonTwelveHelper} from "../src/HellContract.sol";
+
+contract AdvancedThreeTest is Test {
+    AdvancedThree myContract;
+    address user = makeAddr("user");
+    address challengeContract = 0x643315C9Be056cDEA171F4e7b2222a4ddaB9F88D;
+    LessonTwelveHelper hellContract;
+
+    function setUp() public {
+        myContract = new AdvancedThree(user);
+        vm.prank(user);
+        hellContract = new LessonTwelveHelper();
+    }
+
+    function testSolveChallengeContract() public {
+        vm.prank(user);
+
+        string memory twitterHandle = "@0x_Money";
+        bytes memory callData =
+            abi.encodeWithSignature("solveChallenge(address,string)", address(myContract), twitterHandle);
+        (bool success,) = challengeContract.call(callData);
+        success;
+    }
+}
